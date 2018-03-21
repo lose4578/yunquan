93a9bc9f3dfff37ef3ad83981c747c0495fb95e1
Emiyaaaaa <2914034404@qq.com>
2018-03-21 16:42:46 +0800

    测试2

--- yunquan/settings.py
+++ yunquan/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+#lalalalalalala
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
