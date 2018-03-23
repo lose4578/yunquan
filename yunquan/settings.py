5aa90811df1665d40946b63ab1e503a1a6775d55
yulocal <1196423555@qq.com>
2018-03-23 19:38:18 +0800

    创建user_info的app

--- yunquan/settings.py
+++ yunquan/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'yunquan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lose4578',
+        'USER':"lose4578",
+        'PASSWORD':"lose4578",
+        'HOST':"db4free.net",
+        'PORT':'3307'
     }
 }
 
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
