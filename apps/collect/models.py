61583a3d10c961db4f20401bf8fc0c839e429e7d
lose4578 <445326569@qq.com>
2018-03-23 19:57:38 +0800

    创建 app：news sitting中添加app news、collect 修改app目录结构

--- apps/collect/models.py
+++ apps/collect/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class Collect(models.Model):
-    collect_id = models.IntegerField(default=0, verbose_name=u'收藏编号')
+    collect_id = models.IntegerField(default=0, verbose_name=u'收藏id')
     collect_content_id = models.IntegerField(verbose_name=u'内容原始位置')
     collect_add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
