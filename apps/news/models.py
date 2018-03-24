bf55e30c41c5dbd5517a21f265c8e8906766b30f
Emiyaaaaa <2914034404@qq.com>
2018-03-24 11:56:22 +0800

    添加是否null

--- apps/news/models.py
+++ apps/news/models.py
@@ -8,11 +8,11 @@ from django.db import models
 
 
 class News(models.Model):
-    news_id = models.IntegerField(default=0, verbose_name=u'新闻id')
+    news_id = models.IntegerField(verbose_name=u'新闻id')
     news_user = models.ForeignKey(UserMessage, verbose_name=u'用户')
     news_name = models.CharField(max_length=30, verbose_name=u'新闻标题')
-    news_text = models.TextField(verbose_name=u'新闻内容（文字）')
-    news_img = models.ImageField(upload_to='image/%Y/%m', default=u'image/default.png', max_length=100)
+    news_text = models.TextField(null=True,verbose_name=u'新闻内容（文字）')
+    news_img = models.ImageField(null=True,upload_to='image/%Y/%m', default=u'image/default.png', max_length=100)
     news_add_time = models.DateTimeField(default=datetime.now)
     
     class Meta:
