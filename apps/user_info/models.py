e8233b0c45faaf1139e10d961d4a947b8d54532c
Emiyaaaaa <2914034404@qq.com>
2018-03-25 10:06:49 +0800

    为所有外键添加 on_delete=models.deletion.CASCADE

--- apps/user_info/models.py
+++ apps/user_info/models.py
@@ -18,7 +18,7 @@ class UserMessage(models.Model):
     user_img = models.ImageField(upload_to='image/%Y/%m', default=u"image/default.png", max_length=100)
     user_add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
     user_passwd = models.CharField(max_length=50, verbose_name=u'登录密码')
-    user_collect = models.ForeignKey(Collect, verbose_name='用户收藏')
+    user_collect = models.ForeignKey(Collect, on_delete=models.deletion.CASCADE,verbose_name='用户收藏')
 
     class Meta:
         verbose_name = u"个人信息"
