3b07c6887299ab6aca9d4ff838fb71e750fef107
AZ <ZYAZP@users.noreply.github.com>
2018-04-17 21:16:30 +0800

    好友列表后台实现

--- apps/friends_list/adminx.py
+++ apps/friends_list/adminx.py
@@ -2,7 +2,7 @@ import xadmin
 from .models import UserFriendsList
 from xadmin import views
 class UserFriendsListAdmin(object):
-    list_display=['news_name','news_id','news_user']
+    list_display=['user_id','user_friends_list','moments_add_time']
     search_fields=['user_friends_list','moments_add_time']
     list_filter=['user_friends_list','moments_add_time']
 
