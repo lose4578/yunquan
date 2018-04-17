baa2c8f93aea54244678e5da687548451eb7fa1a
AZ <ZYAZP@users.noreply.github.com>
2018-04-17 18:37:33 +0800

    修正一个小bug

--- apps/news/adminx.py
+++ apps/news/adminx.py
@@ -3,7 +3,7 @@ from .models import News
 from xadmin import views
 class NewsAdmin(object):
     list_display=['news_name','news_id','news_user']
-    search_fields=['news_name','news_id','news_user']
+    search_fields=['news_name','news_id']
     list_filter=['news_name','news_id','news_user']
 
 xadmin.site.register(News,NewsAdmin)
