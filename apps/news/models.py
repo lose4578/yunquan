e5456f60682b7cb1b745948c2259681a65b46ae7
Emiyaaaaa <2914034404@qq.com>
2018-04-18 17:40:20 +0800

    将ckeditor加到extra_apps里

--- apps/news/models.py
+++ apps/news/models.py
@@ -16,7 +16,6 @@ class News(models.Model):
     news_user = models.ForeignKey(UserMessage, on_delete=models.deletion.CASCADE,verbose_name=u'用户')
     news_name = models.CharField(max_length=30, verbose_name=u'新闻标题')
     news_text = RichTextField(verbose_name="新闻内容")
-    # news_text = UEditorField(verbose_name=u"新闻内容", width=600, height=300, imagePath="courses/ueditor/", filePath="courses/ueditor/",default='')
     news_img = models.ImageField(verbose_name="新闻封面",null=True,upload_to='image/%Y/%m', default=u'image/default.png', max_length=100)
     news_add_time = models.DateTimeField(verbose_name="上传时间",default=datetime.now)
 
