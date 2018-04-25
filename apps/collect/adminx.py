3135d21f7c16835f4b844f35dda297c61361d34d
zzy <dfg87854d6@126.com>
2018-04-25 19:22:44 +0800

    xadmin update

--- apps/collect/adminx.py
+++ apps/collect/adminx.py
@@ -2,9 +2,9 @@ import xadmin
 from .models import Collect
 from xadmin import views
 class CollectAdmin(object):
-    list_display=['collect_content_id','collect_user_id','collect_add_time']
-    search_fields=['collect_content_id','collect_user_id','collect_add_time']
-    list_filter=['collect_content_id','collect_user_id','collect_add_time']
+    list_display=['collect_content_id','collect_user_id','add_time']
+    search_fields=['collect_content_id','collect_user_id','add_time']
+    list_filter=['collect_content_id','collect_user_id','add_time']
 
 xadmin.site.register(Collect,CollectAdmin)
 
--- apps/user_info/adminx.py
+++ apps/user_info/adminx.py
@@ -2,13 +2,40 @@ import xadmin
 from .models import UserMessage
 from xadmin.plugins.auth import UserAdmin
 from xadmin import views
-class UserMessageAdmin(object):
-    list_display=['user_name','user_email','user_gender','user_add_time']
-    search_fields=['user_name','user_id','user_gender','user_add_time']
-    list_filter=['user_name','user_id','user_gender','user_add_time']
+from crispy_forms.layout import Fieldset
+from xadmin.layout import Main, Row, Side
+from django.utils.translation import ugettext as _
+# class UserMessageAdmin(object):
+#     list_display=['user_name','user_email','user_gender','user_add_time']
+#     search_fields=['user_name','user_id','user_gender','user_add_time']
+#     list_filter=['user_name','user_id','user_gender','user_add_time']
 
-# class UserMessageAdmin(UserAdmin):
-#     pass
+class UserMessageAdmin(UserAdmin):
+    def get_form_layout(self):
+        self.form_layout = (
+            Main(
+                Fieldset('',
+                         'username', 'password',
+                         css_class='unsort no_title'
+                         ),
+                Fieldset(_('Personal info'),
+                         Row('first_name', 'last_name'),
+                         'email'
+                         ),
+                Fieldset(_('Permissions'),
+                         'groups', 'user_permissions'
+                         ),
+                Fieldset(_('Important dates'),
+                         'last_login', 'date_joined'
+                         ),
+            ),
+            Side(
+                Fieldset(_('Status'),
+                         'is_active',  'is_superuser',
+                         ),
+            )
+        )
+        return super(UserAdmin, self).get_form_layout()
 
 #以下代码为后台管理系统的主题，logo配置代码
 
@@ -21,8 +48,8 @@ class GlobalSettings(object):
     site_footer='第四组'
     menu_style='accordion'
 
-xadmin.site.unregister(UserMessage)
-xadmin.site.register(UserMessage,UserMessageAdmin)
+# xadmin.site.unregister(UserMessage)
+# xadmin.site.register(UserMessage,UserMessageAdmin)
 xadmin.site.register(views.BaseAdminView,BaseSetting)
 xadmin.site.register(views.CommAdminView,GlobalSettings)
 
