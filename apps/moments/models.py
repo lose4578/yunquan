2bb226ef8d925642f2590893e8d5768204f21f98
AZ <33780133+ZYAZP@users.noreply.github.com>
2018-05-09 19:38:33 +0800

    Update models.py
    update

--- apps/moments/models.py
+++ apps/moments/models.py
@@ -6,12 +6,11 @@ from user_info.models import UserMessage
 
 
 class Moments(models.Model):
-    moments_id = models.IntegerField(null=False, verbose_name=u'说说id')
     moments_user = models.ForeignKey(UserMessage,verbose_name=u'用户')
     moments_text = models.TextField(verbose_name=u'说说正文')
     moments_img = models.ImageField(upload_to='image/%Y/%m', default=u'image/default.png', max_length=100,verbose_name=u"图片信息")
-    moments_like = models.IntegerField(verbose_name=u'点赞数')
-    moments_comment = models.TextField(verbose_name=u'评论')
+    moments_like = models.IntegerField(default=0,verbose_name=u'点赞数')
+    moments_comment = models.TextField(null=True,verbose_name=u'评论')
     has_delete = models.BooleanField(default=False, verbose_name=u"是否已删除")
     moments_add_time = models.DateTimeField(default=datetime.now, verbose_name=u"说说添加时间")
     user_friends_id = models.TextField(null=True, verbose_name=u"用户好友ID")
@@ -21,4 +20,4 @@ class Moments(models.Model):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return '{0}({1})'.format(self.moments_id, self.moments_user)
+        return '{0}({1})'.format(self.id, self.moments_user)
