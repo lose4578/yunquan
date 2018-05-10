284afcc20ef38c617172af269fd1bd3ca0791ffd
AZ <33780133+ZYAZP@users.noreply.github.com>
2018-05-10 20:39:13 +0800

    Update adminx.py
    update

--- apps/moments/adminx.py
+++ apps/moments/adminx.py
@@ -2,8 +2,8 @@ import xadmin
 from .models import Moments
 from xadmin import views
 class MomentsAdmin(object):
-    list_display=['moments_like','moments_id','moments_user']
-    search_fields=['moments_like','moments_id','moments_user']
-    list_filter=['moments_like','moments_id','moments_user']
+    list_display=['moments_like','id','moments_user']
+    search_fields=['moments_like','id','moments_user']
+    list_filter=['moments_like','id','moments_user']
 
 xadmin.site.register(Moments,MomentsAdmin)
