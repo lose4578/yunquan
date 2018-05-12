37769667032885acfc0c783bb20e5e990718bfd8
AZ <33780133+ZYAZP@users.noreply.github.com>
2018-05-12 21:33:36 +0800

    Update views.py
    update

--- apps/moments/views.py
+++ apps/moments/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.http import JsonResponse
-
+from django.db import connection
 import re
 import time
 import json
@@ -10,16 +10,18 @@ from function import format
 from .forms import AddMomentsForm
 from .models import Moments
 from user_info.models import UserMessage
+from friends_list.models import UserFriendsList
 # Create your views here.
 
 
 class AddMomentsView(View):
     def post(self, request):
-        moments_user = request.POST.get("user", "")
-        moments_text = request.POST.get("text", "")
-        moments_img = request.POST.get("img", "")
+        moments_user = int(request.POST.get('user_id'))
+        moments_text = request.POST.get('text')
+        moments_img = request.POST.get('img')
         moments = Moments()
-        moments.moments_user = moments_user
+        user = UserMessage.objects.get(id = moments_user)
+        moments.moments_user = user
         moments.moments_text = moments_text
         moments.moments_img = moments_img
         moments.save()
@@ -29,62 +31,78 @@ class AddMomentsView(View):
 
 class ShowMomentsView(View):
     def post(self, request):
-        moments_user = request.POST.get("user", "")
-
-        user_message = UserMessage.objects.get(user_id = moments_user)
-        user_message = list(user_message)
-        user_message = format(user_message)
-        friend_ids = user_message[1]['user_friends_id']
-        friend_ids = re.findall("([\s\S]*?),", friend_ids)
-        all_friend_moments = []
-        for n in friend_ids:
-            friend_moments = Moments.objects.filter(moments_user = friend_ids[n])
-            friend_moments = list(friend_moments)
-            friend_moments = format(friend_moments)
-            friend_moments = list(friend_moments.values())
-            all_friend_moments += friend_moments
-
-        moments = Moments.objects.filter(moments_user = moments_user)
-        moments = list(moments)
-        moments = format(moments)
-        moments = list(moments.values())
-        moments += all_friend_moments
+        moments_user = int(request.POST.get('user_id'))
+        moments = Moments.objects.filter(moments_user_id = moments_user)
+        moments_users = []
+        moments_texts = []
+        moments_likes = []
         for i in moments:
-            for j in range(0, len(moments)-1):
-                add_time1 = moments[j]['add_time']
-                add_time2 = moments[j+1]['add_time']
-                timeArray1 = time.strptime(add_time1, "%Y-%m-%d %H:%M:%S")
-                timeArray2 = time.strptime(add_time2, "%Y-%m-%d %H:%M:%S")
-                timestamp1 = time.mktime(timeArray1)
-                timestamp2 = time.mktime(timeArray2)
-                if timestamp1 > timestamp2:
-                    temp = moments[j]
-                    moments[j] = moments[j+1]
-                    moments[j+1] = temp
-        result = moments
+            moments_user = moments[i].moments_user
+            moments_text = moments[i].moments_text
+            moments_like = moments[i].moments_like
+            moments_users += moments_user
+            moments_texts += moments_text
+            moments_likes += moments_like
+
+
+        # user_message = list(user_message)
+        # user_message = format(user_message)
+        friends_list = UserFriendsList.objects.filter(user_id = moments_user)
+        # all_friend_moments = []
+        # for n in friend_ids:
+        #     friend_moments = Moments.objects.filter(moments_user = friend_ids[n])
+        #     friend_moments = list(friend_moments)
+        #     friend_moments = format(friend_moments)
+        #     friend_moments = list(friend_moments.values())
+        #     all_friend_moments += friend_moments
+        #
+        # moments = Moments.objects.filter(moments_user = moments_user)
+        # moments = list(moments)
+        # moments = format(moments)
+        # moments = list(moments.values())
+        # moments += all_friend_moments
+        # for i in moments:
+        #     for j in range(0, len(moments)-1):
+        #         add_time1 = moments[j]['add_time']
+        #         add_time2 = moments[j+1]['add_time']
+        #         timeArray1 = time.strptime(add_time1, "%Y-%m-%d %H:%M:%S")
+        #         timeArray2 = time.strptime(add_time2, "%Y-%m-%d %H:%M:%S")
+        #         timestamp1 = time.mktime(timeArray1)
+        #         timestamp2 = time.mktime(timeArray2)
+        #         if timestamp1 > timestamp2:
+        #             temp = moments[j]
+        #             moments[j] = moments[j+1]
+        #             moments[j+1] = temp
+
+
+        result = {
+            'moments_users': moments_users,
+            'moments_texts': moments_texts,
+            'moments_likes': moments_likes,
+        }
         return JsonResponse(result)
 
 
 class LikeView(View):
     def post(self, request):
-        moments_id = request.POST.get("moments_id", "")
-        moment = Moments.objects.get(moments_id = moments_id)
+        moments_id = request.POST.get("moments_id")
+        moment = Moments.objects.get(id = moments_id)
         moment.moments_like += 1
         moment.save()
         like_num = moment.moments_like
-        result = {'result': like_num}
+        result = {'like_num': like_num}
         return JsonResponse(result)
 
 
 class AddCommentView(View):
     def post(self, request):
-        moments_id = request.POST.get('moments_id')
-        comment_user_id = request.POST.get('user_id')
-        user = UserMessage.objects.get(user_id = comment_user_id)
-        comment_user_name = user.user_name
+        moments_id = int(request.POST.get('moments_id'))
+        comment_user_id = int(request.POST.get('user_id'))
+        user = UserMessage.objects.get(id = comment_user_id)
+        comment_user_name = user.username
         comment = request.POST.get('comment')
-        comment = '~~' + comment_user_name + '``' + comment + '~~'
-        moment = Moments.objects.get(moments_id = moments_id)
+        comment = comment_user_name + '``' + comment + '~~'
+        moment = Moments.objects.get(id = moments_id)
         moment.moments_comment += comment
         moment.save()
         return JsonResponse({
@@ -94,8 +112,8 @@ class AddCommentView(View):
 
 class ShowCommentView(View):
     def post(self, request):
-        moments_id = request.POST.get('moments_id')
-        moment = Moments.objects.get(moments_id= moments_id)
+        moments_id = int(request.POST.get('moments_id'))
+        moment = Moments.objects.get(id= moments_id)
         all_comments = moment.moments_comment
         users = re.findall('~~([\s\S]*?)``', all_comments)
         comments = re.findall('``([\s\S]*?)~~', all_comments)
